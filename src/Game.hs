2667200c1c079079ea62c10e04972c8ec1dd279d
Maty228 <39215496@cuni.cz>
2026-07-02 22:33:50 +0200

    Add applyAction and nextHeadPosition

--- src/Game.hs
+++ src/Game.hs
@@ -1,37 +1,48 @@
 module Game where
-    import Types
 
-    turnLeft :: Direction -> Direction
-    turnLeft North = West
-    turnLeft East = North
-    turnLeft South = East
-    turnLeft West = South
+import Types
 
-
-    turnRight :: Direction -> Direction
-    turnRight North = East
-    turnRight East = South
-    turnRight South = West
-    turnRight West = North
+turnLeft :: Direction -> Direction
+turnLeft North = West
+turnLeft East = North
+turnLeft South = East
+turnLeft West = South
 
 
-    moveForward :: Position -> Direction -> Position
-    moveForward (x,y) North = (x, y-1)
-    moveForward (x,y) East = (x+1, y)
-    moveForward (x,y) South = (x, y+1)
-    moveForward (x,y) West = (x-1, y)
+turnRight :: Direction -> Direction
+turnRight North = East
+turnRight East = South
+turnRight South = West
+turnRight West = North
+
+
+moveForward :: Position -> Direction -> Position
+moveForward (x,y) North = (x, y-1)
+moveForward (x,y) East = (x+1, y)
+moveForward (x,y) South = (x, y+1)
+moveForward (x,y) West = (x-1, y)
 
 
 
 {-
-    Using Image coordination system:
-    0,0 1,0 2,0 3,0 4,0
-    0,1 1,1 2,1 3,1 4,1
-    0,2 1,2 2,2 3,2 4,2
-    0,3 1,3 2,3 3,3 4,3
-    0,4 1,4 2,4 3,4 4,4
+Using Image coordination system:
+0,0 1,0 2,0 3,0 4,0
+0,1 1,1 2,1 3,1 4,1
+0,2 1,2 2,2 3,2 4,2
+0,3 1,3 2,3 3,3 4,3
+0,4 1,4 2,4 3,4 4,4
 -}
 
 -- Returns the head position of a worm, the first position in wormBody is always considered as the head
-    wormHead :: Worm -> Position
-    wormHead worm = head (wormBody worm)
+wormHead :: Worm -> Position
+wormHead worm = head (wormBody worm)
+
+
+applyAction :: Direction -> Action -> Direction
+applyAction dir TurnLeft = turnLeft dir
+applyAction dir TurnRight = turnRight dir
+applyAction dir GoStraight = dir
+
+
+nextHeadPosition :: Worm -> Position
+nextHeadPosition worm = moveForward (wormHead worm) (wormDirection worm)
--- src/Types.hs
+++ src/Types.hs
@@ -1,43 +1,43 @@
 module Types where
 
 
-    type Position = (Int, Int)
+type Position = (Int, Int)
 
-    data Direction
-        = North
-        | East
-        | South
-        | West
-        deriving (Show, Eq, Ord, Enum, Bounded)
-
-
-    data Action
-        = TurnLeft
-        | GoStraight
-        | TurnRight
-        deriving (Show, Eq, Ord, Enum, Bounded)
-
-    data Tile
-        = Wall
-        | Empty
-        | Food
-        | Poison
-        deriving (Show, Eq)
-
-    data Worm = Worm
-        {
-            wormId :: Int,
-            wormBody :: [Position],
-            wormDirection :: Direction,
-            wormAlive :: Bool
-        }
-        deriving (Show, Eq)
+data Direction
+    = North
+    | East
+    | South
+    | West
+    deriving (Show, Eq, Ord, Enum, Bounded)
 
 
+data Action
+    = TurnLeft
+    | GoStraight
+    | TurnRight
+    deriving (Show, Eq, Ord, Enum, Bounded)
 
-    data GameState = GameState
-        {
-            gameWorms :: [Worm],
-            gameTick :: Int
-        }
-        deriving (Show, Eq)
+data Tile
+    = Wall
+    | Empty
+    | Food
+    | Poison
+    deriving (Show, Eq)
+
+data Worm = Worm
+    {
+        wormId :: Int,
+        wormBody :: [Position],
+        wormDirection :: Direction,
+        wormAlive :: Bool
+    }
+    deriving (Show, Eq)
+
+
+
+data GameState = GameState
+    {
+        gameWorms :: [Worm],
+        gameTick :: Int
+    }
+    deriving (Show, Eq)
