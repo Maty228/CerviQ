39b8f995642959a1c0325abb08675832b5d19b6d
Maty228 <39215496@cuni.cz>
2026-07-03 12:06:21 +0200

    Add map and worm collision helpers

--- src/Game.hs
+++ src/Game.hs
@@ -3,6 +3,10 @@ module Game where
 import Types
 import Maps
 
+--
+-- Direction and position helpers
+--
+
 turnLeft :: Direction -> Direction
 turnLeft North = West
 turnLeft East = North
@@ -34,6 +38,12 @@ Using Image coordination system:
 0,4 1,4 2,4 3,4 4,4
 -}
 
+
+--
+-- Worm helpers
+--
+
+
 -- Returns the head position of a worm
 -- An alive worm always has a non-empty body and the first position is its head
 wormHead :: Worm -> Position
@@ -54,12 +64,21 @@ nextHeadPosition worm = moveForward (wormHead worm) (wormDirection worm)
 
 
 
--- Build a new worm body after moving to a new head position
+removeTail :: [Position] -> [Position]
+removeTail [_] = []
+removeTail (segment : rest) = segment : removeTail rest
+removeTail [] = []
+
+--  | Build a new worm body after moving to a new head position
 -- If the worm grows, the tail is kept, otherwise the last body segment is removed
 advanceBody :: Bool -> Position -> [Position] -> [Position]
-advanceBody grows newHead oldBody
-    | grows = newHead : oldBody
-    | otherwise = newHead : init oldBody
+advanceBody grows newHead oldBody =
+    case oldBody of
+        [] -> [newHead]
+        _
+            | grows -> newHead : oldBody
+            | otherwise -> newHead : removeTail oldBody
+
 
 moveWorm :: Bool -> Worm -> Worm
 moveWorm grows worm =
@@ -73,14 +92,74 @@ moveWormAfterAction grows action worm =
     in moveWorm grows turnedWorm
 
 
+--
+-- Map collision helpers
+--
+
 
 isBlocked :: GameMap -> Position -> Bool
-isBlocked map pos =
-    case tileAt map pos of
+isBlocked gameMap pos =
+    case tileAt gameMap pos of
         Nothing -> True
         Just tile -> tile == Wall 
 
 
 wouldHitWall :: GameMap -> Worm -> Bool
-wouldHitWall map worm =
-    isBlocked map (nextHeadPosition worm)
+wouldHitWall gameMap worm =
+    isBlocked gameMap (nextHeadPosition worm)
+
+
+wouldHitWallAfterAction :: GameMap -> Action -> Worm -> Bool
+wouldHitWallAfterAction gameMap action worm =
+    let newDirection = applyAction (wormDirection worm) action
+        turnedWorm = worm {wormDirection = newDirection}
+    in wouldHitWall gameMap turnedWorm
+
+
+occupiedPositions :: [Worm] -> [Position]
+occupiedPositions worms =
+    concatMap wormBody worms
+
+
+futureWorms :: [(Bool, Action, Worm)] -> [Worm]
+futureWorms moves =
+    map (\(grows, action, worm) -> moveWormAfterAction grows action worm) moves
+
+futureOccupiedPositions :: [(Bool, Action, Worm)] -> [Position]
+futureOccupiedPositions moves =
+    occupiedPositions (futureWorms moves)
+
+
+countPosition :: Position -> [Position] -> Int
+countPosition pos positions =
+    length (filter (== pos) positions)
+
+headCollision :: [Position] -> Worm -> Bool
+headCollision positions worm =
+    countPosition (wormHead worm) positions > 1
+
+positionOccupied :: Position -> [Position] -> Bool
+positionOccupied pos positions =
+    pos `elem` positions
+
+wormCollidesWithBodies :: [Worm] -> Worm -> Bool
+wormCollidesWithBodies worms worm =
+    headCollision (occupiedPositions worms) worm
+
+wormCollidesWithMap :: GameMap -> Worm -> Bool
+wormCollidesWithMap gameMap worm =
+    isBlocked gameMap (wormHead worm)
+
+
+wormCollides :: GameMap -> [Worm] -> Worm -> Bool
+wormCollides gameMap worms worm =
+    wormCollidesWithMap gameMap worm ||
+    wormCollidesWithBodies worms worm
+
+
+simulateTurn :: GameMap -> [(Bool, Action, Worm)] -> ([Worm], [Worm])
+simulateTurn gameMap moves =
+    let movedWorms = futureWorms moves
+        collidingWorms = filter (wormCollides gameMap movedWorms) movedWorms
+        survivingWorms = filter (not . wormCollides gameMap movedWorms) movedWorms
+    in (survivingWorms, collidingWorms)
